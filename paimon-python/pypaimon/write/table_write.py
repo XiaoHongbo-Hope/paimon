c2b8ee038fc25ff53859dba899446a80e7587070
xiaohongbo <xiaohongbo.xhb@alibaba-inc.com>
2026-01-13 15:27:30 +0800

    rename write_raydata to write_ray to be consistent withe write_pandas and write_arrow

--- paimon-python/pypaimon/write/table_write.py
+++ paimon-python/pypaimon/write/table_write.py
@@ -68,7 +68,7 @@ class TableWrite:
         self.file_store_write.write_cols = write_cols
         return self
 
-    def write_raydata(self, dataset, overwrite=False, parallelism=1):
+    def write_ray(self, dataset, overwrite=False, parallelism=1):
         from pypaimon.write.ray_datasink import PaimonDatasink
         datasink = PaimonDatasink(self.table, overwrite=overwrite)
         dataset.write_datasink(datasink, concurrency=parallelism)
