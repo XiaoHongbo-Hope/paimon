55b82fab34773b3c6ca85191683567397ac63c0c
xiaohongbo <xiaohongbo.xhb@alibaba-inc.com>
2026-02-28 11:51:25 +0800

    fix system field not nullable issue

--- paimon-python/pypaimon/read/reader/format_pyarrow_reader.py
+++ paimon-python/pypaimon/read/reader/format_pyarrow_reader.py
@@ -24,6 +24,7 @@ from pyarrow import RecordBatch
 
 from pypaimon.common.file_io import FileIO
 from pypaimon.read.reader.iface.record_batch_reader import RecordBatchReader
+from pypaimon.table.special_fields import SpecialFields
 
 
 class FormatPyArrowReader(RecordBatchReader):
@@ -85,7 +86,8 @@ class FormatPyArrowReader(RecordBatchReader):
                     column_idx = self.missing_fields.index(field_name)
                     col_type = _type_for_missing(field_name)
                     all_columns.append(missing_columns[column_idx])
-                    out_fields.append(pa.field(field_name, col_type, nullable=True))
+                    nullable = not SpecialFields.is_system_field(field_name)
+                    out_fields.append(pa.field(field_name, col_type, nullable=nullable))
             # Create a new RecordBatch with all columns
             return pa.RecordBatch.from_arrays(all_columns, schema=pa.schema(out_fields))
 
