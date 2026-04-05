7c0ef413c14847e67038342da8ecdf3e9b91f908
xiaohongbo <1346652787@qq.com>
2026-04-05 17:01:13 +0800

    simplify lance read optimize: keep batch_size and close, remove unnecessary changes

--- paimon-python/pypaimon/read/reader/format_lance_reader.py
+++ paimon-python/pypaimon/read/reader/format_lance_reader.py
@@ -13,12 +13,11 @@
 #  distributed under the License is distributed on an "AS IS" BASIS,
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
-# limitations under the License.
+#  limitations under the License.
 ################################################################################
 
 from typing import List, Optional, Any
 
-import pyarrow as pa
 import pyarrow.dataset as ds
 from pyarrow import RecordBatch
 
@@ -28,70 +27,40 @@ from pypaimon.read.reader.lance_utils import to_lance_specified
 
 
 class FormatLanceReader(RecordBatchReader):
-    """Streaming Lance file reader with lazy initialization."""
+    """A Format Reader that reads record batches from a Lance file."""
 
     def __init__(self, file_io: FileIO, file_path: str, read_fields: List[str],
                  push_down_predicate: Any, batch_size: int = 1024):
-        self._file_io = file_io
-        self._file_path = file_path
-        self._read_fields = read_fields
-        self._push_down_predicate = push_down_predicate
-        self._batch_size = batch_size
-
-        self._reader = None
-        self._lance_file_reader = None
-        self._initialized = False
-
-    def _ensure_initialized(self):
-        if self._initialized:
-            return
-
         import lance
 
-        file_path_for_lance, storage_options = to_lance_specified(
-            self._file_io, self._file_path)
+        file_path_for_lance, storage_options = to_lance_specified(file_io, file_path)
 
-        columns = self._read_fields if self._read_fields else None
-        self._lance_file_reader = lance.file.LanceFileReader(
+        columns = read_fields if read_fields else None
+        self._lance_reader = lance.file.LanceFileReader(
             file_path_for_lance,
             storage_options=storage_options,
-            columns=columns,
-        )
+            columns=columns)
+        reader_results = self._lance_reader.read_all(batch_size=batch_size)
 
-        reader_results = self._lance_file_reader.read_all(
-            batch_size=self._batch_size)
-        self._reader = reader_results.to_batches()
-        self._initialized = True
+        if push_down_predicate is not None:
+            pa_table = reader_results.to_table()
+            in_memory_dataset = ds.InMemoryDataset(pa_table)
+            scanner = in_memory_dataset.scanner(filter=push_down_predicate, batch_size=batch_size)
+            self._reader = scanner.to_reader()
+        else:
+            self._reader = reader_results.to_batches()
 
     def read_arrow_batch(self) -> Optional[RecordBatch]:
-        if not self._initialized:
-            self._ensure_initialized()
-
-        while True:
-            try:
-                if hasattr(self._reader, 'read_next_batch'):
-                    batch = self._reader.read_next_batch()
-                else:
-                    batch = next(self._reader)
-            except StopIteration:
-                return None
-
-            if batch is None:
-                return None
-
-            if self._push_down_predicate is not None:
-                table = pa.Table.from_batches([batch])
-                filtered = ds.InMemoryDataset(table).scanner(
-                    filter=self._push_down_predicate).to_table()
-                if filtered.num_rows == 0:
-                    continue
-                return filtered.combine_chunks().to_batches()[0]
-
-            return batch
+        try:
+            if hasattr(self._reader, 'read_next_batch'):
+                return self._reader.read_next_batch()
+            else:
+                return next(self._reader)
+        except StopIteration:
+            return None
 
     def close(self):
-        if self._lance_file_reader is not None:
-            self._lance_file_reader.close()
-            self._lance_file_reader = None
+        if self._lance_reader is not None:
+            self._lance_reader.close()
+            self._lance_reader = None
         self._reader = None
-        self._initialized = False
